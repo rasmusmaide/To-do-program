984888e611eeec5045fcae0ab7606e598cf8aec5
Rasmus <rasmusmaide@gmail.com>
2018-04-06 17:07:09 +0300

    added getters-setters-constructors

--- src/main/java/listfiles/Task.java
+++ src/main/java/listfiles/Task.java
@@ -1,13 +1,37 @@
 package listfiles;
 
 public class Task {
-    private String date;
+    private String deadline;
     private String description;
     private boolean done;
 
-    public Task(String date, String description, boolean done) {
-        this.date = date;
+    public Task(String deadline, String description, boolean done) {
+        this.deadline = deadline;
         this.description = description;
+        this.done = done;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
         this.done = done;
     }
 }
--- src/main/java/listfiles/Todo_list.java
+++ src/main/java/listfiles/Todo_list.java
@@ -4,5 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Todo_list {
-    List<Task> tasks = new ArrayList<>();
+    private List<Task> tasks;
+    private String description;
+
+    public Todo_list(List<Task> tasks, String description) {
+        this.tasks = tasks;
+        this.description = description;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
