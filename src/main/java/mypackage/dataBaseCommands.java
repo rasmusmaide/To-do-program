b2cc3bad82c74d0bdd4922b655a79b9e10bac615
karelpaan <Karel.Paan@tudeng.ut.ee>
2018-04-06 23:41:34 +0300

    thirdCommit

--- src/main/java/mypackage/dataBaseCommands.java
+++ src/main/java/mypackage/dataBaseCommands.java
@@ -1,4 +1,44 @@
 package mypackage;
 
+// #goodreads --> https://www.tutorialspoint.com/h2_database/h2_database_create.htm
+
+//Some code reminders, mainly for me
+//---
+//DROP TABLE TUTORIALS_TBL
+//***
+//CREATE TABLE tutorials_tbl (
+//id INT NOT NULL,
+//title VARCHAR(50) NOT NULL,
+//author VARCHAR(20) NOT NULL,
+//submission_date DATE,
+//);
+//***
+
 public class dataBaseCommands {
+
+    public static void main(String[] args) {
+
+    }
+
 }
+
+
+//The code I have used so far...
+//---
+//Creating th table:
+//CREATE TABLE ToDo_s (
+//        id INT AUTO_INCREMENT PRIMARY KEY,
+//        creation_date TIMESTAMP,
+//        due_date TIMESTAMP,
+//        headline VARCHAR(100),
+//    text VARCHAR,
+//    done BOOLEAN,
+//);
+
+//Inserting values to table:
+//INSERT INTO todo_s VALUES (1,'2005-01-12 08:02:00','2005-01-12 08:02:00','My note','some very random text', FALSE)
+
+/** IMPORTANT!
+Advanced insert, where id is automatically placed
+INSERT INTO todo_s(CREATION_DATE, DUE_DATE , HEADLINE, TEXT, DONE) VALUES ( '2005-01-12 08:02:00', '2005-01-12 08:02:00', 'jkl', 'asd', 'FALSE')
+ */
