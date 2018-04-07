1d12d3d65aceede03d26a8fd3c72cfe2d1a10c80
karelpaan <Karel.Paan@tudeng.ut.ee>
2018-04-08 02:55:23 +0300

    fourthCommit

--- src/main/java/mypackage/dataBaseCommands.java
+++ src/main/java/mypackage/dataBaseCommands.java
@@ -14,10 +14,66 @@ package mypackage;
 //);
 //***
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Boolean.getBoolean;
+
 public class dataBaseCommands {
 
-    public static void main(String[] args) {
+    Connection conn;
 
+    public static void main(String[] args) throws Exception {
+        Class.forName("org.h2.Driver");
+        dataBaseCommands db = new dataBaseCommands(
+                /** CHANGE THIS!!!
+                 * AND RUN H2 FIRST */
+                "jdbc:h2:tcp://localhost/C:\\Users\\Karel\\Desktop\\To-do-program\\src\\todoBase");
+        System.out.println(db.getAllTasks());
+    }
+
+
+    dataBaseCommands(String dataBaseURL) throws SQLException {
+        conn = DriverManager.getConnection(dataBaseURL);
+        dataBaseCommands[] dataBase = new dataBaseCommands[1];
+        // Siin tuleks luua parameetrina antud URL-i põhjal Connection objekt ja salvestada see isendimuutujasse.
+        // Draiveri laadimist ei pea siin enam tegema!
+    }
+
+    List<List<String>> getAllTasks() throws SQLException {
+
+
+        List<List<String>> allTasks = new ArrayList<List<String>>();
+
+        PreparedStatement context = conn.prepareStatement("select * from TODO_S");
+
+        ResultSet rs = context.executeQuery();
+
+        while (rs.next()) {
+            List<String> oneTask = new ArrayList<String>();
+
+
+            oneTask.add(Integer.toString(rs.getInt("id")));
+
+            oneTask.add(rs.getTimestamp("creation_date").toString());
+
+            oneTask.add(rs.getTimestamp("due_date").toString());
+
+            oneTask.add(rs.getString("headline"));
+
+            oneTask.add(rs.getString("text"));
+
+            oneTask.add(Boolean.toString(getBoolean("headline")));
+
+            allTasks.add(oneTask);
+
+        }
+        rs.close();
+
+        context.close();
+
+        return allTasks;
     }
 
 }
@@ -38,7 +94,8 @@ public class dataBaseCommands {
 //Inserting values to table:
 //INSERT INTO todo_s VALUES (1,'2005-01-12 08:02:00','2005-01-12 08:02:00','My note','some very random text', FALSE)
 
-/** IMPORTANT!
-Advanced insert, where id is automatically placed
-INSERT INTO todo_s(CREATION_DATE, DUE_DATE , HEADLINE, TEXT, DONE) VALUES ( '2005-01-12 08:02:00', '2005-01-12 08:02:00', 'jkl', 'asd', 'FALSE')
+/**
+ * IMPORTANT!
+ * Advanced insert, where id is automatically placed
+ * INSERT INTO todo_s(CREATION_DATE, DUE_DATE , HEADLINE, TEXT, DONE) VALUES ( '2005-01-12 08:02:00', '2005-01-12 08:02:00', 'jkl', 'asd', 'FALSE')
  */
