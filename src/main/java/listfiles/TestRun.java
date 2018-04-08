99f0a4c23c352f10127e356d2b300f24b97504d6
Fred Hansen <ffredhansen@gmail.com>
2018-04-08 22:59:01 +0300

    põhifunktsioonid

--- src/main/java/listfiles/TestRun.java
+++ src/main/java/listfiles/TestRun.java
@@ -25,22 +25,27 @@ public class TestRun {
             while (true) {
                 System.out.println("Enter command: ");
                 String str = scanner.nextLine();
-                
+
                 switch (str) {
                     case "exit":
                         System.out.println("Bye!");
                         break label;
                     case "show list":
                         System.out.println(dbc.getAllTasks());
-
                         break;
-                    case "add":
-                        System.out.println("placeholder");
-
+                    case "add":   // 2005-01-12 08:03:00;2005-01-12 08:02:00;juust;kapsas;TRUE
+                        System.out.println("Add task: "); // ("2005-01-12 08:03:00", "2005-01-12 08:02:00", "pealkiri", "tegevus", FALSE);
+                        String[] tasktext = scanner.nextLine().split(";"); // ";" error potential
+                        Task ntask = new Task(tasktext[0], tasktext[1], tasktext[2], tasktext[3], Boolean.parseBoolean(tasktext[4]));
+                        dbc.addTask(ntask);
+                        System.out.println("added task: " + ntask.toString());
+                        //System.out.println(dbc.getAllTasks());
                         break;
                     case "delete":
-                        System.out.println("placeholder");
-
+                        System.out.println("Enter index: ");
+                        int index = Integer.parseInt(scanner.nextLine());
+                        dbc.deleteTask(index);
+                        System.out.println("Task deleted successfully.");
                         break;
                     default:
                         System.out.println("Not a command!");
