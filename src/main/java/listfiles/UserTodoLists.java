150edb014c146a88db12d4ebdd2f84d4d656fbe4
Fred Hansen <ffredhansen@gmail.com>
2018-05-03 16:37:00 +0300

    Fixed the problem with gson2

---
+++ src/main/java/listfiles/UserTodoLists.java
@@ -0,0 +1,15 @@
+package listfiles;
+
+import java.util.List;
+
+public class UserTodoLists {
+    private List<Todo_list> userTodoLists;
+
+    public List<Todo_list> getUserTodoLists() {
+        return userTodoLists;
+    }
+
+    public void setUserTodoLists(List<Todo_list> userTodoLists) {
+        this.userTodoLists = userTodoLists;
+    }
+}
